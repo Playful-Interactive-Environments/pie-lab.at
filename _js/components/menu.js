c5e3e1b05d42a265b51ec08c42e1d1f96b71ac11
Thomas Heller <dev@thomasheller.net>
2016-01-17 10:39:22 +0100

    remove jQuery dependency

--- _js/components/menu.js
+++ _js/components/menu.js
@@ -1,17 +1,14 @@
-const $ = require('jQuery');
-
 module.exports = (function menu() {
   let navList;
 
   const handleMenuClick = () => {
-    navList.toggleClass('nav_open');
+    navList.classList.toggle('nav_open');
   };
 
   const initMenu = () => {
-    $(document).ready(() => {
-      $('.menu_toggle_button').on('click', handleMenuClick);
-    });
-    navList = $('.nav_list');
+    let togglebutton = document.getElementById('menu_toggle_button');
+    navList = document.getElementById('nav_list');
+    togglebutton.addEventListener('click', handleMenuClick);
   };
 
   return {
