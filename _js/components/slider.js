9aec15e6a969072cc5f6e84b94d334a5134cceef
Thomas Heller <dev@thomasheller.net>
2016-01-17 11:39:11 +0100

    configure slider options

--- _js/components/slider.js
+++ _js/components/slider.js
@@ -3,7 +3,14 @@ const Imageslider = require('../vendor/ideal-image-slider');
 module.exports = (function slider() {
 
   const initSlider = () => {
-    new Imageslider.Slider('#heroslider');
+    const slider = new Imageslider.Slider({
+      selector: '#hero-slider',
+      interval: 10000,
+      transitionDuration: 750,
+      previousNavSelector: '#hero-slider-prev',
+      nextNavSelector: '#hero-slider-next',
+    });
+    slider.start();
   };
 
   return {
