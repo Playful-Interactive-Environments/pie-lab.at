fcd3f500bc8dff552458b0abe8b7db05b0770154
Thomas Heller <dev@thomasheller.net>
2016-01-29 17:46:53 +0100

    switched image slider to fickety

--- _js/components/slider.js
+++ _js/components/slider.js
@@ -1,16 +1,5 @@
-const Imageslider = require('../vendor/ideal-image-slider');
-
 module.exports = (function slider() {
-
   const initSlider = () => {
-    const slider = new Imageslider.Slider({
-      selector: '#hero-slider',
-      interval: 10000,
-      transitionDuration: 750,
-      previousNavSelector: '#hero-slider-prev',
-      nextNavSelector: '#hero-slider-next',
-    });
-    slider.start();
   };
 
   return {
