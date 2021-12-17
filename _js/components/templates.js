975261c7a9ee223906227ecb16a19f371c5b2733
Wolfgang Hochleitner <wolfgang@hochleitner.cc>
2021-12-17 09:22:34 +0100

    Fixed videos not autoplaying anymore
    Browsers now require `allow="autoplay"` as an attribute for the iFrame in order to autoplay videos.

--- _js/components/templates.js
+++ _js/components/templates.js
@@ -1,4 +1,4 @@
-const youtube = (id) => `<iframe src="https://www.youtube.com/embed/${id}?autoplay=true" frameborder="0" allowfullscreen></iframe>`;
+const youtube = (id) => `<iframe src="https://www.youtube.com/embed/${id}?autoplay=1" frameborder="0" allow="autoplay" allowfullscreen></iframe>`;
 const vimeo = (id) => `<iframe src="https://player.vimeo.com/video/${id}?badge=0" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>`;
 
 module.exports = {
